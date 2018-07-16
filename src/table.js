5c05090b02f7fccf69b9bc17b72ac0cd442d7311
Matt Lewis <matt@mplewis.com>
2018-07-15 19:06:22 -0600

    feat: colWidths

--- src/table.js
+++ src/table.js
@@ -1,6 +1,11 @@
-import {curry} from 'ramda'
+import {curry, length, map, max, pipe, reduce, transpose} from 'ramda'
+
+const maxIn = reduce(max, -Infinity)
+const lengthOfLongest = pipe(map(length), maxIn)
 
 export const wrap = curry((outer, count, inner) => {
   if (count <= 0) return inner
   return wrap(outer, count - 1, outer + inner + outer)
 })
+
+export const colWidths = table => transpose(table).map(lengthOfLongest)
--- src/table.test.js
+++ src/table.test.js
@@ -1,9 +1,16 @@
-import {wrap} from './table'
+import { colWidths, wrap } from './table'
 
 describe('wrap', () => {
   test('wraps strings', () => {
     expect(wrap('#', 1, 'notice')).toBe('#notice#')
     const wrapper1 = wrap('!', 2)
     expect(wrapper1('danger')).toBe('!!danger!!')
+  })
+})
+
+describe('colWidths', () => {
+  test('determines the correct column widths', () => {
+    const table = [['name', 'id'], ['Virginia Augustus', '1']]
+    expect(colWidths(table)).toEqual([17, 2])
   })
 })
